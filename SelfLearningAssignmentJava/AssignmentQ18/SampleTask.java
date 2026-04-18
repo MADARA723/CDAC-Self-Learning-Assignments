47508d16f5632a1d9e84b97d2eb59934aa5f13f6
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-04-18 23:45:26 +0530

    Add files via upload

---
+++ SelfLearningAssignmentJava/AssignmentQ18/SampleTask.java
@@ -0,0 +1,14 @@
+package AssignmentQ18;
+
+public class SampleTask extends Thread {
+	
+	public void run()
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(Thread.currentThread().getName()+":"+i) ;
+		}
+		
+	}
+
+}
